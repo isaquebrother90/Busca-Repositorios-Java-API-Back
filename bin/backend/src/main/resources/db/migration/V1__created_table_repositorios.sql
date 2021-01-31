8d0f080c6fa3515341fa59e62bba066ffbfb7d61
isaquebrother90 <improgramcode@gmail.com>
2021-01-31 14:32:43 -0300

    Delete unecessary files

--- bin/backend/src/main/resources/db/migration/V1__created_table_repositorios.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS repositorios (
-  id int(11) PRIMARY KEY AUTO_INCREMENT,
-  image varchar(200) DEFAULT NULL,
-  name varchar(150) NOT NULL,
-  stars int(40) NOT NULL,
-  forks int(40) NOT NULL
-);
